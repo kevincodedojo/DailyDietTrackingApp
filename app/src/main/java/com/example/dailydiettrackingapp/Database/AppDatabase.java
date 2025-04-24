6f9f597680fa869061b1a3cb8cc0ffeaf379dabb
kevincodedojo <153450889+kevincodedojo@users.noreply.github.com>
2025-04-23 23:00:40 -0700

    update AppDatabase, DietTipDAO

--- app/src/main/java/com/example/dailydiettrackingapp/Database/AppDatabase.java
+++ app/src/main/java/com/example/dailydiettrackingapp/Database/AppDatabase.java
@@ -2,15 +2,21 @@ package com.example.dailydiettrackingapp.Database;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 import com.example.dailydiettrackingapp.Database.entities.DietTip;
 import com.example.dailydiettrackingapp.Database.entities.Meal;
 
-@Database(entities = {Meal.class, DietTip.class}, version = 1,exportSchema = false)
+
+@Database(entities = {Meal.class, DietTip.class}, version = 2,exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
-    public static final String DIET_TIP_TABLE = ;
+    public static final String DIET_TIP_TABLE = "dietTipTable";
 
     public abstract MealDAO mealDAO();
 
     public abstract DietTipDAO dietTipDAO();
+
+
+
+
 }
