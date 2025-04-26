7596c168fae33af13d8bb17009c4c1f1a3225fea
bevangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2025-04-26 00:05:37 -0700

    Created Converters.. in converters added date from long data type and it converts to new Date data

--- app/src/main/java/com/example/dailydiettrackingapp/data/Converters.java
+++ app/src/main/java/com/example/dailydiettrackingapp/data/Converters.java
@@ -1,4 +1,19 @@
 package com.example.dailydiettrackingapp.data;
 
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
 public class Converters {
+
+    @TypeConverter
+    public static Date fromTimestamp(Long value) {
+        if (value == null) {
+            return null;
+        } else {
+            return new Date(value);
+        }
+    }
+
+
 }
