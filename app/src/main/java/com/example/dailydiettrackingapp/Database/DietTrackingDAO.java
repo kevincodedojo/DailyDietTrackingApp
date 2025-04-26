d647b9c47ee6559a4a2925f261aee7d7005d21b9
bevangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2025-04-26 14:57:27 -0700

    added DAO and query

--- app/src/main/java/com/example/dailydiettrackingapp/Database/DietTrackingDAO.java
+++ app/src/main/java/com/example/dailydiettrackingapp/Database/DietTrackingDAO.java
@@ -1,4 +1,22 @@
 package com.example.dailydiettrackingapp.Database;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.dailydiettrackingapp.Database.entities.DietTracking;
+
+import java.util.List;
+
+@Dao
 public interface DietTrackingDAO {
+
+    @Insert
+    void insert(DietTracking dietTracking);
+
+    @Query("SELECT * FROM diet_tracking_table")
+    List<DietTracking> getAllDietTrackingRecords();
+
+    @Query("DELETE FROM diet_tracking_table WHERE id = :id")
+    void deleteById(int id);
 }
