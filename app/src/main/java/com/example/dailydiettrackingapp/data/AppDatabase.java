ebc043d04a7596dc977b5f2bc7cff94535be73f5
bevangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2025-04-25 21:57:53 -0700

    created login dao

--- app/src/main/java/com/example/dailydiettrackingapp/data/AppDatabase.java
+++ app/src/main/java/com/example/dailydiettrackingapp/data/AppDatabase.java
@@ -2,7 +2,9 @@ package com.example.dailydiettrackingapp.data;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-@Database(entities = {Meal.class}, version = 1)
+
+@Database(entities = {Meal.class, Login.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract MealDAO mealDAO();
+    public abstract LoginDAO loginDAO();
 }
