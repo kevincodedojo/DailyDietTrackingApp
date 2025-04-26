528e8efef0a1401a1ab5020e15fc54f2f76a2dc1
Aida <aida.png@gmail.com>
2025-04-26 14:12:04 -0700

    implemented LoginActivity, added room db integration

---
+++ app/src/main/java/com/example/dailydiettrackingapp/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.example.dailydiettrackingapp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import com.example.dailydiettrackingapp.Database.AppDatabase;
+import com.example.dailydiettrackingapp.Database.UserDAO;
+import com.example.dailydiettrackingapp.Database.entities.User;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText, passwordEditText;
+    private Button loginButton;
+    private UserDAO userDAO;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usernameEditText = findViewById(R.id.editTextUsername);
+        passwordEditText = findViewById(R.id.editTextPassword);
+        loginButton = findViewById(R.id.buttonLogin);
+
+        AppDatabase db = Room.databaseBuilder(
+                getApplicationContext(),
+                AppDatabase.class,
+                "app-database").allowMainThreadQueries().build();
+        userDAO = db.userDAO();;
+
+        loginButton.setOnClickListener(v -> {
+            String username = usernameEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT).show();
+            } else {
+                User user = new User(username, password);
+                userDAO.insert(user);
+                Toast.makeText(this, "User registered successfuly!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
