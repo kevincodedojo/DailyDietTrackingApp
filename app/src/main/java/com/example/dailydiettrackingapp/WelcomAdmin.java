fcb251eaa7114af6efc0dc7738f788b1e2d9c411
kevincodedojo <153450889+kevincodedojo@users.noreply.github.com>
2025-04-25 22:24:24 -0700

    Link to Adding diet tips from WelcomAdmin

--- app/src/main/java/com/example/dailydiettrackingapp/WelcomAdmin.java
+++ app/src/main/java/com/example/dailydiettrackingapp/WelcomAdmin.java
@@ -1,6 +1,9 @@
 package com.example.dailydiettrackingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,18 @@ public class WelcomAdmin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcom_admin);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button addDietTipsButton = findViewById(R.id.addDietTipsButton);
+        addDietTipsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomAdmin.this, AddingDietTips.class);
+                startActivity(intent);
+            }
         });
+
+
+
     }
 }
