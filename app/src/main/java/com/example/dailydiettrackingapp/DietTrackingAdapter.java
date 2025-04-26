b5efac5ad85280da728275f9981e144873b37931
bevangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2025-04-26 16:33:30 -0700

    setting up landing page to display data from dietTracking

---
+++ app/src/main/java/com/example/dailydiettrackingapp/DietTrackingAdapter.java
@@ -0,0 +1,59 @@
+package com.example.dailydiettrackingapp.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.dailydiettrackingapp.R;
+import com.example.dailydiettrackingapp.Database.entities.DietTracking;
+
+import java.util.List;
+
+public class DietTrackingAdapter extends RecyclerView.Adapter<DietTrackingAdapter.ViewHolder> {
+
+    private List<DietTracking> dietTrackingList;
+
+    public DietTrackingAdapter(List<DietTracking> dietTrackingList) {
+        this.dietTrackingList = dietTrackingList;
+    }
+
+    public void setDietTrackingList(List<DietTracking> dietTrackingList) {
+        this.dietTrackingList = dietTrackingList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_diet_tracking, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        DietTracking dietTracking = dietTrackingList.get(position);
+        holder.mealNameTextView.setText(dietTracking.getMealName());
+        holder.caloriesTextView.setText("Calories: " + dietTracking.getCalories());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dietTrackingList == null ? 0 : dietTrackingList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView mealNameTextView;
+        TextView caloriesTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mealNameTextView = itemView.findViewById(R.id.mealNameTextView);
+            caloriesTextView = itemView.findViewById(R.id.caloriesTextView);
+        }
+    }
+}
