553a3647f54e511ac38efab77b3f480be4f1787c
bevangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2025-04-26 20:59:40 -0700

    added comments

--- app/src/androidTest/java/com/example/dailydiettrackingapp/DietTrackingDatabaseTest.java
+++ app/src/androidTest/java/com/example/dailydiettrackingapp/DietTrackingDatabaseTest.java
@@ -21,6 +21,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 //thjs test is for the diet tracking database
+
 // it checks if the diet tracking data is inserted correctly
 
 @RunWith(AndroidJUnit4.class)
